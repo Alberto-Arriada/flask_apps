603fc40aa8893faa8d85b742ea6aa0feff56a62c
Alberto Arriada <60834729+Alberto-Arriada@users.noreply.github.com>
2020-07-08 19:36:36 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask_migrate import Migrate
+from flask_restful import Api
+from Config import Config
+from extensions import db
+from resources.user import UserListResource
+from resources.resourcerecipe import RecipeListResource, RecipeResource, RecipePublishResource
+
+
+#Function to create flask app
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    register_extensions(app) #initializes SQLAlchemy and setup Flask-Migrate
+    register_resources(app) #setup resource routing
+    return app
+
+
+def register_extensions(app):
+    db.init_app(app)
+    migrate = Migrate(app, db)
+
+
+def register_resources(app):
+    api = Api(app)
+    api.add_resource(UserListResource, '/users')
+    api.add_resource(RecipeListResource, '/recipes')
+    api.add_resource(RecipeResource, '/recipes/<int:recipe_id>')
+    api.add_resource(RecipePublishResource, '/recipes/<int:recipe_id>/publish')
+
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run()
